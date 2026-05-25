7f620675e99bdad6db187743a2aea711879521f6
lyrshift3 <lyr_v@qq..com>
2026-05-25 10:44:21 +0800

    update

--- controller/index.js
+++ controller/index.js
@@ -1,2 +1,1 @@
 export { getUsers, getUser, addUser, updateUser, deleteUser, login, register } from './userController.js'
-export { getVideos, addVideo, updateVideo } from './videoController.js'
--- router/index.js
+++ router/index.js
@@ -1,10 +1,8 @@
 import express from 'express'
 import userRouter from './user.js'
-import videoRouter from './video.js'
 
 const router = express.Router()
 
 router.use('/user', userRouter)
-router.use('/video', videoRouter)
 
 export default router
